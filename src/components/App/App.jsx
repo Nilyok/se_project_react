1b4c69e5b5ad3dfab59e6e5e593eddedaae6dc2d
Nilyok <nilyok.ch@gmail.com>
2025-09-22 00:08:14 -0500

    correct from auto detected

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import { useState, useEffect } from "react";
 import { getWeather } from "../../utils/weatherApi";
 import Header from "../Header/Header";
@@ -6,6 +7,7 @@ import Footer from "../Footer/Footer";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import ItemModal from "../ItemModal/ItemModal";
 import { defaultClothingItems } from "../../utils/clothingItems";
+
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
