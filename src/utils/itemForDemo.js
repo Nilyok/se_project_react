294dcce941461366f08e4b27bd31ac04ddef3e0a
Nilyok <nilyok.ch@gmail.com>
2025-10-15 17:12:07 -0500

    ready before demo

---
+++ src/utils/itemForDemo.js
@@ -0,0 +1,34 @@
+  const itemForDemo = [
+  
+  {
+    _id: 16,
+    name: "T-Shirt",
+    weather: "hot",
+    link:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/T-Shirt.png?etag=44ed1963c44ab19cd2f5011522c5fc09",
+  },
+
+  {
+    _id: 14,
+    name: "Sunglasses",
+    weather: "hot",
+    link:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Sunglasses.png?etag=a1bced9e331d36cb278c45df51150432",
+  },
+
+  {
+    _id: 10,
+    name: "Scarf",
+    weather: "cold",
+    link:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Scarf.png?etag=74efbee93810c926b5507e862c6cb76c",
+  },
+    
+    {
+    _id: 5,
+    name: "Hoodie",
+    weather: "cold",
+    link:
+      "https://example.com/notfound-image.jpg",
+  },
+];
